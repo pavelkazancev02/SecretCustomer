3f9046def3adcb2bc3d37994f8f4543852d0b62e
pavelkazancev02 <pavelkazancev02@gmail.com>
2020-05-06 23:37:53 +0300

    + Feedback Layouts

--- app/src/main/java/com/example/secretcustomer/MainActivity.kt
+++ app/src/main/java/com/example/secretcustomer/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_shops_finish);
+        setContentView(R.layout.activity_shops_feedback);
     }
 }
