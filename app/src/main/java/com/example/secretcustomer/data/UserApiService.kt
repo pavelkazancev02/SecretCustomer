b7a27cdde4abe8bfdcf0a1588fc30e028413632d
Victoria Rotaru <vica.rotaru97@gmail.com>
2020-05-11 13:07:10 +0300

    add password

--- app/src/main/java/com/example/secretcustomer/data/UserApiService.kt
+++ app/src/main/java/com/example/secretcustomer/data/UserApiService.kt
@@ -64,6 +64,7 @@ data class CreateUserPostData(
     @SerializedName("firstName") var firstName: String,
     @SerializedName("lastName") var lastName: String,
     @SerializedName("email") var email: String,
-    @SerializedName("phone") var phone: String,
+    @SerializedName("password") var password: String,
+    @SerializedName("phone") var phone: String?,
     @SerializedName("role") var role: String // Should be changed to Customer
 )
