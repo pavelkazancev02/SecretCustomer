67937622a0cf96186dddc5a6a3733985096b1c75
KN878 <kalinskiy98@gmail.com>
2020-05-12 21:06:08 +0300

    feat: to intent command

--- app/src/main/java/com/example/secretcustomer/util/NavigationCommand.kt
+++ app/src/main/java/com/example/secretcustomer/util/NavigationCommand.kt
@@ -1,8 +1,10 @@
 package com.example.secretcustomer.util
 
+import android.content.Intent
 import androidx.navigation.NavDirections
 
 sealed class NavigationCommand {
     data class To(val directions: NavDirections): NavigationCommand()
+    data class ToIntent(val intent: Intent): NavigationCommand()
     object Back: NavigationCommand()
 }
