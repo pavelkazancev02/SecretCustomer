3df6891cbf2675d49586d4f800f85cae742951fc
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2020-05-13 11:28:42 +0300

    feat: add customer activity with navigation

---
+++ app/src/main/java/com/example/secretcustomer/ui/fragments/MyFeedbackFragment.kt
@@ -0,0 +1,23 @@
+package com.example.secretcustomer.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.secretcustomer.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MyFeedbackFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_feedback, container, false)
+    }
+
+}
